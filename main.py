cd55662a47f16968d3654ec2febf25cc86aba56c
ehsan2793 <ehsan2793@gmail.com>
2022-02-26 15:06:24 -0800

    more fnctionality added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 import pandas as p
 
 img = './us_map.gif'
@@ -9,14 +10,18 @@ screen.bgpic(img)
 data = p.read_csv('50_states.csv')
 all_states = data['state'].tolist()
 print(data)
-
-answer = screen.textinput(title='Guess the State', prompt='Whats another state')
-if answer in all_states:
-    stateInfo = data[data.state == answer]
-    jim = turtle.Turtle()
-    jim.hideturtle()
-    jim.penup()
-    jim.goto(int(stateInfo.x), int(stateInfo.y))
-    jim.write(answer)
+guessed_states = []
+while len(guessed_states) < len(all_states):
+    answer = screen.textinput(title=f'{len(guessed_states)} / 50 Guess the State', prompt='Whats another state').title()
+    if answer in all_states:
+        stateInfo = data[data.state == answer]
+        time.sleep(0.2)
+        jim = turtle.Turtle()
+        jim.hideturtle()
+        jim.penup()
+        jim.goto(int(stateInfo.x), int(stateInfo.y))
+        jim.write(answer)
+        guessed_states.append(answer)
+        time.sleep(1)
 
 screen.exitonclick()
