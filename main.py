b9620c1e08d3f5acd7bb883f29dce07b022ab002
ehsan2793 <ehsan2793@gmail.com>
2022-02-26 16:55:25 -0800

    game completed

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ screen.bgpic(img)
 
 data = p.read_csv('50_states.csv')
 all_states = data['state'].tolist()
-print(data)
 guessed_states = []
 while len(guessed_states) < len(all_states):
     answer = screen.textinput(title=f'{len(guessed_states)} / 50 Guess the State', prompt='Whats another state').title()
+    if answer == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = p.DataFrame(missing_states)
+        new_data.to_csv('state_to_learn.csv')
+
+        break
     if answer in all_states:
         stateInfo = data[data.state == answer]
         time.sleep(0.2)
@@ -23,5 +31,17 @@ while len(guessed_states) < len(all_states):
         jim.write(answer)
         guessed_states.append(answer)
         time.sleep(1)
+
+fill_out = True
+while fill_out:
+    for state in all_states:
+        stateInfo = data[data.state == state]
+        jim = turtle.Turtle()
+        jim.hideturtle()
+        jim.penup()
+        jim.color('red')
+        jim.goto(int(stateInfo.x), int(stateInfo.y))
+        jim.write(state)
+    fill_out = False
 
 screen.exitonclick()
---
+++ readme.md
@@ -0,0 +1,12 @@
+Can you tell me name of all the states? 
+here look at this before testing your knowledge 
+
+![](Screen%20Shot%202022-02-26%20at%204.50.35%20PM.png)
+
+
+# game:
+In this game you will see the empty map and it will tell you to write the name of states
+see if you can get them all
+
+![](us_map.gif)
+
